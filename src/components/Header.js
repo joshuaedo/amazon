c236cbae2b69830a0f1b7310a57953c9146ac799
Joshua Edo <joshua.edo01@gmail.com>
2023-05-28 16:59:25 +0000

    Update 3 files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,27 @@
+import Image from "next/image";
+
 export default function Header() {
-  return <>Header</>;
+  return (
+    <>
+      {/* Top Nav */}
+      <nav className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="flex mt-2 items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        
+        {/* SearchBar */}
+        <div>
+          <input type="text " />
+        </div>
+      </nav>
+      {/* Bottom Nav */}
+      <nav></nav>
+    </>
+  );
 }
