7716d87a10dd9ec52ff257b00773440836df49ee
Joshua <joshua.edo01@gmail.com>
2023-06-04 09:26:01 +0100

    more refactoring

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['links.papareact.com', 'fakestoreapi.com', 'm.media-amazon.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,8 +17,14 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        colors: {
+          amazon_blue: {
+            light: '#232F3E',
+            DEFAULT: '#131921',
+          },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/line-clamp')],
+};
