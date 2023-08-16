0681bc78d7fd2aca58f0dcc997e8e918ab1b3095
Joshua Edo <joshua.edo01@gmail.com>
2023-08-16 18:05:17 +0000

    add session as a prop

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import MetaHead from "../components/MetaHead";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
 import Footer from "@/components/Footer";
+import { getSession } from "next-auth/react";
 
 export default function Home({ products }) {
   return (
@@ -19,6 +20,7 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
@@ -26,6 +28,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       products,
+      session,
     },
   };
 }
--- pages/orders.js
+++ pages/orders.js
@@ -110,6 +110,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       orders,
+      session,
     },
   };
 }
